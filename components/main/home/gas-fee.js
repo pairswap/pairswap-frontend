0d6702fff66c4a0364670cf48d16ff697dd50c87
Phat Vo <vphat8415@gmail.com>
2022-04-13 00:19:28 +0700

    chore: move proxy endpoint to config

--- components/main/home/gas-fee.js
+++ components/main/home/gas-fee.js
@@ -1,27 +1,36 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-import useAsync from 'hooks/useAsync';
+import useError from 'hooks/useError';
 import useChain from 'hooks/useChain';
 import useWeb3 from 'hooks/useWeb3';
 import { getGasFeeInToken } from 'request/rest';
 import { convertNumberToString } from 'utils/transform';
 
 function GasFee() {
+  const [gasCost, setGasCost] = useState(null);
   const { srcChain, srcToken } = useChain();
-  const { connected } = useWeb3();
-  const { execute, value } = useAsync(getGasFeeInToken);
+  const setError = useError();
+  const { connected, supported } = useWeb3();
 
   useEffect(() => {
-    if (connected && srcChain && srcChain.transferName && srcToken && srcToken.symbol) {
-      execute({ chain: srcChain.transferName, tokenId: srcToken.symbol });
+    if (connected && supported && srcChain?.transferName && srcToken?.symbol) {
+      getGasFeeInToken({ chain: srcChain.transferName, tokenId: srcToken.symbol })
+        .then((value) => {
+          if (value?.gas_cost) {
+            setGasCost(value.gas_cost);
+          }
+        })
+        .catch((error) => setError(error, { silent: true }));
+    } else {
+      setGasCost(null);
     }
-  }, [execute, connected, srcChain, srcToken]);
+  }, [connected, supported, srcChain, srcToken, setError]);
 
   return (
     <div>
-      {value ? (
+      {gasCost ? (
         <span>
-          Gas fee: <strong>{convertNumberToString(value.gas_cost)}</strong>
+          Gas fee: <strong>{convertNumberToString(gasCost)}</strong>
         </span>
       ) : null}
     </div>
--- request/rest.js
+++ request/rest.js
@@ -1,18 +1,31 @@
-const baseURL = 'http://localhost:8080';
+let proxyAPI;
+
+if (typeof window !== 'undefined' && typeof window.config !== 'undefined') {
+  proxyAPI = window.config.proxyAPI;
+}
 
 export async function getGasFeeInToken({ chain, tokenId }) {
-  return fetch(`${baseURL}/getGasFeeInToken?chain=${chain}&token_id=${tokenId}`).then(
-    (response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
+  if (!proxyAPI) return Promise.reject('No proxy API');
+
+  try {
+    const response = await fetch(`${proxyAPI}/getGasFeeInToken?chain=${chain}&token_id=${tokenId}`);
+
+    if (!response.ok) {
+      throw Error(response.statusText);
     }
-  );
+
+    const { data } = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function support({ name, email, txURL, comment }) {
-  return fetch(`${baseURL}/support`, {
+  if (!proxyAPI) return Promise.reject('No proxy API');
+
+  return fetch(`${proxyAPI}/support`, {
     method: 'POST',
     body: JSON.stringify({ name, email, tx_url: txURL, comment }),
   }).then((response) => {
