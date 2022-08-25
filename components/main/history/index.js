8905f10f807e661aa2ce50b2bb7f3c95cdb2fea9
frog <95737177+frogchocolatebarpie@users.noreply.github.com>
2022-08-25 14:31:51 +0700

    fix: align row

--- components/main/history/index.js
+++ components/main/history/index.js
@@ -11,14 +11,20 @@ const limit = 10;
 
 function History() {
   const [histories, setHistories] = useState(null);
-  const [offset, setOffset] = useState(0);
+  const [page, setPage] = useState(0);
+  const [pages, setPages] = useState(0);
   const { chainInfos } = useChain();
   const { tokenInfos } = useToken();
   const { account } = useWeb3();
 
   useEffect(() => {
     if (account) {
-      getHistory({ address: account }).then(({ data }) => setHistories(data.histories));
+      getHistory({ address: account })
+        .then(({ data }) => {
+          setHistories(data.histories);
+          setPages(Math.ceil(data.histories.length / limit));
+        })
+        .catch((error) => console.error(error));
     } else {
       setHistories(null);
     }
@@ -34,14 +40,14 @@ function History() {
             <div className="history__row-item">Token</div>
             <div className="history__row-item">From</div>
             <div className="history__row-item">To</div>
-            <div className="history__row-item">Amount</div>
+            <div className="history__row-item history__row-item--small">Amount</div>
             <div className="history__row-item">Transaction hash</div>
             <div className="history__row-item">Created at</div>
           </div>
         </div>
         <div className="history__body">
           {histories
-            .slice(limit * offset, limit * offset + limit)
+            .slice(page * limit, page * limit + limit)
             .map(
               (
                 {
@@ -56,7 +62,7 @@ function History() {
                 index
               ) => (
                 <div key={index} className="history__row">
-                  <div className="history__row-item">
+                  <div className="history__row-item history__row-item--left">
                     <img
                       src={tokenInfos[tokenSymbol].icon}
                       alt="token"
@@ -64,7 +70,7 @@ function History() {
                     />
                     <span className="history__name">{tokenSymbol}</span>
                   </div>
-                  <div className="history__row-item">
+                  <div className="history__row-item history__row-item--left">
                     <img
                       src={chainInfos[srcChain].icon}
                       alt="source chain"
@@ -72,7 +78,7 @@ function History() {
                     />
                     <span className="history__name">{chainInfos[srcChain].name}</span>
                   </div>
-                  <div className="history__row-item">
+                  <div className="history__row-item history__row-item--left">
                     <img
                       src={chainInfos[destChain].icon}
                       alt="source chain"
@@ -80,7 +86,7 @@ function History() {
                     />
                     <span className="history__name">{chainInfos[destChain].name}</span>
                   </div>
-                  <div className="history__row-item">{amount}</div>
+                  <div className="history__row-item history__row-item--small">{amount}</div>
                   <div className="history__row-item">
                     <a href={srcLink} target="_blank" rel="noreferrer" className="history__link">
                       {shortenHash(srcHash)}
@@ -93,17 +99,24 @@ function History() {
         </div>
       </div>
       <div className="pagination">
-        {Math.floor(histories.length / limit) > 0
-          ? new Array(Math.ceil(histories.length / limit)).fill().map((_, index) => (
-              <button
-                key={index}
-                onClick={() => setOffset(index)}
-                className={classname('page', index === offset && 'page--active')}
-              >
-                {index + 1}
-              </button>
-            ))
-          : null}
+        {pages > 1 ? (
+          <>
+            <button
+              onClick={() => setPage((prevState) => prevState - 1)}
+              disabled={page === 0}
+              className={classname(page === 0 && 'page--disabled')}
+            >
+              <img src="/images/chevron-left.svg" alt="previous" />
+            </button>
+            <button
+              onClick={() => setPage((prevState) => prevState + 1)}
+              disabled={page + 1 >= pages}
+              className={classname(page + 1 >= pages && 'page--disabled')}
+            >
+              <img src="/images/chevron-right.svg" alt="next" />
+            </button>
+          </>
+        ) : null}
       </div>
     </main>
   );
