4a3ce80e548ff980283dc1bcf65999c1a6aa9ef3
frog <95737177+frogchocolatebarpie@users.noreply.github.com>
2022-09-23 12:21:19 +0700

    feat: random tokens

--- contexts/token.js
+++ contexts/token.js
@@ -1,19 +1,23 @@
 import { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import useLocalStorage from 'hooks/useLocalStorage';
-
 export const TokenContext = createContext();
 export const TokenContextUpdate = createContext();
+
+function randomToken(tokens) {
+  const index = Math.floor(Math.random() * tokens.length);
+  return tokens[index];
+}
 
 function TokenProvider({ children }) {
   const [tokens, setTokens] = useState(null);
   const [tokenInfos, setTokenInfos] = useState(null);
-  const [token, setToken] = useLocalStorage('token', null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     if (tokens && tokens.length > 0 && (!token || !tokens.includes(token))) {
-      setToken(tokens[0]);
+      const newToken = randomToken(tokens);
+      setToken(newToken);
     }
   }, [tokens, token, setToken]);
 
