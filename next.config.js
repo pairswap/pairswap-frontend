4cfa10a6bcaadd4bcf1788935c3dfffeeb1fce59
frog <95737177+frogchocolatebarpie@users.noreply.github.com>
2022-10-17 15:06:11 +0700

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'x-dns-prefetch-control',
+            value: 'on',
+          },
+          {
+            key: 'strict-transport-security',
+            value: 'max-age=63072000; includesubdomains; preload',
+          },
+          {
+            key: 'x-xss-protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 };
